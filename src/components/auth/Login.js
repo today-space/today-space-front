cb1664e8e45b1ae9829fd642d185eb007faf1417
ryurbsgks5114 <ryurbsgks5114@naver.com>
2024-07-24 13:29:01 +0900

    feat: 로그인 페이지 UI 구현

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,6 +1,28 @@
+import { Link } from "react-router-dom"
+import Socail from "./Social";
+import "./auth.css";
+
 function Login() {
   return (
-    <></>
+    <div className="auth-component-container">
+      <h2>로그인</h2>
+
+      <input
+        name="username"
+        placeholder="아이디를 입력해주세요" />
+
+      <input
+        name="password"
+        type="password"
+        placeholder="비밀번호를 입력해주세요" />
+
+      <div className="auth-component-container-flex">
+        <Link to="/auth/signup">회원가입</Link>
+        <button>로그인</button>
+      </div>
+
+      <Socail />
+    </div>
   );
 }
 
