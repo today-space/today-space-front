14205c1c101ee74a07498549b83d94a0ce386dde
youngju <ysy561356@naver.com>
2024-07-31 20:07:32 +0900

    ✨ update : app에 게시글 경로 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
 import Auth from "./pages/Auth";
 import Oauth from "./pages/Oauth";
+import Post from "./pages/Post";
 import "./App.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Main />} />
       <Route path="/auth/:id" element={<Auth />} />
       <Route path="/oauth/:id" element={<Oauth />} />
+      <Route path="/post" element={<Post />} />
     </Routes>
   );
 }
