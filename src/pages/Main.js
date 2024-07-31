21aa6a9a68bc918287ace115a8be634befca3808
youngju <ysy561356@naver.com>
2024-07-31 14:27:10 +0900

    ✨ update : 메인화면 인기 게시글, 상품 추가

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,17 @@
 import Topbar from "../components/common/Topbar";
+import PopularPosts from '../components/main/PopularPosts';
+import PopularProducts from '../components/main/PopularProducts';
+import './page.css'; // 스타일을 위한 CSS 파일
 
 function Main() {
   return (
-    <div>
-      <Topbar />
-    </div>
+      <div>
+        <Topbar/>
+        <div className="container">
+          <PopularPosts/>
+          <PopularProducts/>
+        </div>
+      </div>
   );
 }
 
